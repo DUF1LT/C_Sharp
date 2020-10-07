df0fe47901fcfa1a41ab3f97f017af993a8921d7
Vadim <duf1lt@yandex.ru>
2020-10-07 21:58:56 +0300

    Task 1

---
+++ Лаб. 3/Lab 3/Lab 3/Phone.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_3
+{
+    partial class Phone
+    {
+        public const int conversationTimeLimit = 360;
+        private readonly int id;
+        private string surname;
+        private string name;
+        private string patronym;
+        private string address;
+        private int creditCardNumber;
+        private int debit;
+        private int credit;
+        private int cityConversationTime;
+        private int intercityConversationTime;
+        public static int numberOfItems = 0;
+        static Phone() // статический конструктор (инкрементирует кол-во объектов)
+        {
+            numberOfItems++;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set 
+            {
+                name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+        public string Patronym
+        {
+            get
+            {
+                return patronym;
+            }
+            set
+            {
+                patronym = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+        public int CreditCardNumber
+        {
+            get
+            {
+                return creditCardNumber;
+            }
+            set
+            {
+                creditCardNumber = value;
+            }
+        }
+        public int Debit
+        {
+            get
+            {
+                return debit;
+            }
+            set
+            {
+                debit = value;
+            }
+        }
+        public int CityConversationTime
+        {
+            get
+            {
+                return cityConversationTime;
+            }
+        }
+        public int Credit
+        {
+            get
+            {
+                return credit;
+            }
+            set
+            {
+                credit = value;
+            }
+        }
+        public int InterCityConversationTime
+        {
+            get
+            {
+                return intercityConversationTime;
+            }
+        }
+
+        //private Phone() {}
+        public Phone() // конструктор по умолчанию
+        {
+            Random rand = new Random();
+            id = rand.Next(1000, 10000);
+            surname = "undef";
+            name = "undef";
+            patronym = "undef";
+            address = "undef";
+            creditCardNumber = 0;
+            debit = 0;
+            credit = 0;
+            cityConversationTime = 0;
+            intercityConversationTime = 0;
+        }
+
+        public Phone(int id, string surname, string name, string patronym, string address, int creditCardNumber, int debit,
+            int credit, int cityConversationTime, int intercityConversationTime) // конструктор с параметрами
+        {
+            this.id = numberOfItems * 1000 + 20 * numberOfItems / 5;
+            this.surname = surname;
+            this.name = name;
+            this.patronym = patronym;
+            this.address = address;
+            this.creditCardNumber = creditCardNumber;
+            this.debit = debit;
+            this.credit = credit;
+            this.cityConversationTime = cityConversationTime;
+            this.intercityConversationTime = intercityConversationTime;
+        }
+
+        public void Balance()
+        {
+            Console.WriteLine($"Текущий баланс с учетом дебета({debit}) и кредита({credit}) составляет - {debit - credit}");
+        }
+
+    }
+
+    partial class Phone
+    {
+        public static int GetNumberOfItems() // статический метод (возвращает количество созданных объектов данного класса
+        {
+            return numberOfItems;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (this.GetType() != obj.GetType()) return false;
+            return true;
+        }
+        public override int GetHashCode()
+        {
+            int hash = this.id;
+            hash = (hash * 47) + this.GetHashCode();
+            return hash;
+        }
+        public override string ToString()
+        {
+            return "Type " + base.ToString()
+            + id + " "
+            + surname + " "
+            + name + " "
+            + patronym + " "
+            + address + " "
+            + creditCardNumber + " "
+            + debit + " "
+            + credit + " "
+            + cityConversationTime + " "
+            + intercityConversationTime + " ";
+        }
+    }
+
+    
+}
--- Лаб. 3/Lab 3/Lab 3/Program.cs
+++ Лаб. 3/Lab 3/Lab 3/Program.cs
@@ -6,7 +6,39 @@ namespace Lab_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int anountOfItems = Convert.ToInt32(Console.ReadLine());
+            Phone[] phoneArray = new Phone[anountOfItems];
+            for(int i = 0; i < anountOfItems; i++)
+            {
+                phoneArray[i] = new Phone();
+
+                Console.WriteLine("Введите имя абонента: ");
+                phoneArray[i].Name = Console.ReadLine();
+                Console.WriteLine("Введите фамилия абонента: ");
+                phoneArray[i].Surname = Console.ReadLine();
+                Console.WriteLine("Введите отчество абонента: ");
+                phoneArray[i].Patronym = Console.ReadLine();
+                Console.WriteLine("Введите адрес абонента: ");
+                phoneArray[i].Address = Console.ReadLine();
+                Console.WriteLine("Введите номер кредитной карточки абонента: ");
+                phoneArray[i].CreditCardNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите дебет абонента: ");
+                phoneArray[i].Debit = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите кредит абонента: ");
+                phoneArray[i].Credit = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите время городских разговоров абонента: ");
+                phoneArray[i].CityConversationTime = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите время междугородних разговоров абонента: ");
+                phoneArray[i].InterCityConversationTime = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if(anountOfItems > 1)
+                Console.WriteLine($"Объект 1 класса Phone идентичен объекту 2 класса Phone - {phoneArray[1].Equals(phoneArray[2])}");
+
+
+
+            var user = new { Name = "Vadim", Age = "18" };
+            Console.WriteLine($"Имя из анонимного типа - {user.Name}, возраст из анонимного типа {user.Age}");
         }
     }
 }
